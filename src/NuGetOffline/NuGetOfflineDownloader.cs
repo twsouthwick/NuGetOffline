91a615a5e0b14b694426f93e3f8d3c1f5c39bdb5
Taylor Southwick <twsouthwick@outlook.com>
2017-09-27 16:38:30 -0400

    Add style cop and rules

---
+++ src/NuGetOffline/NuGetOfflineDownloader.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace NuGetOffline
+{
+    /// <summary>
+    /// Contains entry point for main application logic called from <see cref="Program.Main(string[])"/>
+    /// </summary>
+    public class NuGetOfflineDownloader
+    {
+        /// <summary>
+        /// Entry point for main application logic called from <see cref="Program.Main(string[])"/>
+        /// </summary>
+        public Task RunAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/NuGetOffline/Program.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using System.Threading.Tasks;
+
+namespace NuGetOffline
+{
+    /// <summary>
+    /// Entry point for applications
+    /// </summary>
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            using (var container = CreateContainer())
+            {
+                await container.Resolve<NuGetOfflineDownloader>().RunAsync();
+            }
+        }
+
+        private static IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<NuGetOfflineDownloader>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            return builder.Build();
+        }
+    }
+}
